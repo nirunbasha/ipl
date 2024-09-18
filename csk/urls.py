866152303eccf85ea9bff63991bf7734afa89f98
nirunbasha <nirunbasha123@gmail.com>
2024-09-18 13:19:03 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from csk.views import *
+urlpatterns = [
+   
+    path('dhoni/',dhoni,name='dhoni')
+]
---
+++ csk/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def dhoni(request):
+    return render(request,'dhoni.html')
+# Create your views here.
